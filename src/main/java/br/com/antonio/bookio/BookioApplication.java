b964982361dcb0c04725ae40e405a3e01fe0580b
Antônio Lima Jr <antonio.df.013@gmail.com>
2021-06-29 04:29:37 -0300

    First Commit

---
+++ src/main/java/br/com/antonio/bookio/BookioApplication.java
@@ -0,0 +1,13 @@
+package br.com.antonio.bookio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookioApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BookioApplication.class, args);
+  }
+
+}
---
+++ src/test/java/br/com/antonio/bookio/BookioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.antonio.bookio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookioApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
